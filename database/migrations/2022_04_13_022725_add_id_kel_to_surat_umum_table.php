582985c9a5de18adea39781fcaa9518299c2509f
spardano <sakti.pardano29@gmail.com>
2022-07-26 11:01:09 +0700

    susah banget

--- database/migrations/2022_04_13_022725_add_id_kel_to_surat_umum_table.php
+++ database/migrations/2022_04_13_022725_add_id_kel_to_surat_umum_table.php
@@ -26,7 +26,7 @@ class AddIdKelToSuratUmumTable extends Migration
     public function down()
     {
         Schema::table('surat_umum', function (Blueprint $table) {
-            Schema::dropColumns('id_kel');
+            $table->dropColumn('id_kel');
         });
     }
 }
