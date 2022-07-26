c4554fd720924c24c7223ba9a9dfa9980c8b475a
spardano <sakti.pardano29@gmail.com>
2022-07-26 10:57:37 +0700

    perbaikan migrasi 2

--- database/migrations/2022_04_13_022725_add_id_kel_to_surat_umum_table.php
+++ database/migrations/2022_04_13_022725_add_id_kel_to_surat_umum_table.php
@@ -26,7 +26,7 @@ class AddIdKelToSuratUmumTable extends Migration
     public function down()
     {
         Schema::table('surat_umum', function (Blueprint $table) {
-            Schema::dropColumns('kode_kel_desa');
+            Schema::dropColumns('id_kel');
         });
     }
 }
